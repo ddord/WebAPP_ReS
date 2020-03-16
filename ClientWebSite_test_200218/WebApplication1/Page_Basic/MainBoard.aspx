995906ca3db6f89ef9aed0b2c17f5ca3a020ead0
cow_ddord <ikihiji11@gmail.com>
2020-03-16 18:05:23 +0900

    c_ddord

--- ClientWebSite_test_200218/WebApplication1/Page_Basic/MainBoard.aspx
+++ ClientWebSite_test_200218/WebApplication1/Page_Basic/MainBoard.aspx
@@ -1,6 +1,40 @@
 ﻿<%@ Page Title="메인 게시판" Language="C#" MasterPageFile="~/NestedLeftSide.Master" AutoEventWireup="true" CodeBehind="MainBoard.aspx.cs" Inherits="WebApplication1.Page_Basic.MainBoard" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContentSub" runat="server">
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#datatable').data
+        });
+    </script>
 
+    <table id="datatable" class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col" class="col-md-1">분류
+                </th>
+                <th scope="col" class="col-md-5">제목
+                </th>
+                <th scope="col" class="col-md-3">글쓴이
+                </th>
+                <th scope="col" class="col-md-3">날짜
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="">
+                <td class="col-md-1" >1</td>
+                <td class="col-md-5">Montería</td>
+                <td class="col-md-3">Colombia</td>
+                <td class="col-md-3">2019.01.02.</td>
+            </tr>
+            <tr>
+                <td class ="col-md-1">2</td>
+                <td class ="col-md-5">Birmingham</td>
+                <td class ="col-md-3">United Kingdom</td>
+                <td class ="col-md-3">2019.01.02.</td>
+            </tr>
+            
+        </tbody>
+    </table>
 </asp:Content>
 
--- ClientWebSite_test_200218/WebApplication1/Page_Basic/MainBoard.aspx.cs
+++ ClientWebSite_test_200218/WebApplication1/Page_Basic/MainBoard.aspx.cs
@@ -1,17 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace WebApplication1.Page_Basic
 {
+    public class MainBoardList
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }                
+        public DateTime WirteDate { get; set; }
+    }
+
     public partial class MainBoard : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        [WebMethod]
+        public void GetEmployees()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            List<MainBoardList> employees = new List<MainBoardList>();
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetEmployees", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    MainBoardList employee = new MainBoardList();
+                    employee.Id = Convert.ToInt32(rdr["Id"]);
+                    employee.Category = rdr["FirstName"].ToString();
+                    employee.Title = rdr["LastName"].ToString();
+                    employee.Name = rdr["Gender"].ToString();    
+                    employee.WirteDate = Convert.ToDateTime(rdr["HireDate"]);
+                    employees.Add(employee);
+                }
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(employees));
         }
     }
 }
